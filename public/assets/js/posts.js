5e35a7bc7678e6f3efe5a668b054ecd5512a29f6
xiaohe <xiaohe@163.com>
2020-03-18 13:29:47 +0800

    完成分页筛选的部分功能

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -1,5 +1,8 @@
-// 发送ajax 请求文章列表
+// 获取分类 和状态 
+var cid = $('#category').val();
+var state = $('#state').val();
 
+// 发送ajax 请求文章列表
 $.ajax({
     type: 'get',
     url: '/posts',
@@ -19,13 +22,16 @@ $.ajax({
     }
 })
 
+// 分页功能
 function changePage(index) {
 
     $.ajax({
         type: 'get',
         url: '/posts',
         data: {
-            page: index
+            page: index,
+            category: cid,
+            state: state
         },
         success: function (res) {
             let html = template('pTpl', { data: res.records });
@@ -38,3 +44,39 @@ function changePage(index) {
     })
 
 }
+
+// 获取所有的文章分类 
+$.ajax({
+    type: 'get',
+    url: '/categories',
+    success: function (res) {
+        let html = template('cTpl', { data: res });
+        // console.log(html);
+        $('#category').append(html);
+    }
+})
+
+// 筛选功能
+
+$('#search').on('click', function () {
+    // 获取分类 和状态 
+    cid = $('#category').val();
+    state = $('#state').val();
+    // 向服务器发送ajax请求 
+    $.ajax({
+        type: 'get',
+        url: '/posts',
+        data: {
+            category: cid,
+            state: state
+        },
+        success: function (res) {
+            let html = template('pTpl', { data: res.records });
+            $('tbody').html(html);
+            // 还要调用 template方法 
+            let pageHtml = template('pageTpl', res);
+            // console.log(pageHtml)
+            $('.pagination').html(pageHtml);
+        }
+    })
+});
