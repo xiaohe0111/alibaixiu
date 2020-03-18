f3d6cd8d2a5a402b20c87c6f7486619212365760
xiaohe <xiaohe@163.com>
2020-03-18 13:15:23 +0800

    完成分页效果功能

--- public/assets/js/posts.js
+++ public/assets/js/posts.js
@@ -11,5 +11,30 @@ $.ajax({
         let html = template('pTpl', { data: res.records });
         console.log(html)
         $('tbody').html(html);
+
+        // 还要调用 template方法 
+        let pageHtml = template('pageTpl', res);
+        // console.log(pageHtml)
+        $('.pagination').html(pageHtml);
     }
 })
+
+function changePage(index) {
+
+    $.ajax({
+        type: 'get',
+        url: '/posts',
+        data: {
+            page: index
+        },
+        success: function (res) {
+            let html = template('pTpl', { data: res.records });
+            $('tbody').html(html);
+
+            let pageHtml = template('pageTpl', res);
+            // console.log(pageHtml)
+            $('.pagination').html(pageHtml);
+        }
+    })
+
+}
--- routes/actions/post/find.js
+++ routes/actions/post/find.js
@@ -1,7 +1,7 @@
 // 用户模块
 const { Post, validatePost } = require('../../../model/Post');
 // 分页
-const pagination  = require('mongoose-sex-page');
+const pagination = require('mongoose-sex-page');
 // 工具
 const _ = require('lodash');
 
@@ -22,7 +22,7 @@ module.exports = async (req, res) => {
 	}
 	console.log(condition)
 	// 查询用户信息
-	const posts = await pagination(Post).page(page).size(10).display(5).find(condition).populate('author', '-password').populate('category').select('-content -meta').exec();
+	const posts = await pagination(Post).page(page).size(2).display(5).find(condition).populate('author', '-password').populate('category').select('-content -meta').exec();
 	// 响应
 	res.send(posts);
 }
